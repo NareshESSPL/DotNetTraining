dff4fea33fdfacb279b30225ac0c0115e71c0cd2
Anubhab Dash <anubhabd@essbbsr.esspl.com>
2025-04-29 00:13:38 +0530

    4th Assignment

---
+++ Assignment_4.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EmployeeManagementSystem
+{
+
+    public sealed class AadharCard
+    {
+        private string AadharNumber;
+
+
+
+        public string Nationality;
+
+        
+        public AadharCard(string AadharNumber, string Nationality)
+        {
+            this.AadharNumber = AadharNumber;
+            this.Nationality= Nationality;  
+        }
+
+       
+        public void AadharDetails()
+        {
+
+
+            Console.WriteLine($"Aadhar Number : {AadharNumber} ------Nationality:  {Nationality}");
+        }
+    }
+
+ 
+    public class AadharGuide
+    {
+        public void AadharCenter()
+        {
+
+            AadharCard A = new AadharCard("1224354", "Indian");
+
+            
+           A.AadharDetails();
+        }
+    }
+}
---
+++ assignment-4.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem
+{
+    public sealed class LoanProfile
+    {
+        public string LoanerName;
+        public int LoanedID;
+        public double LoanAmount;
+        public double AnnualIncome;
+        public int IDCardNo;
+
+        public LoanProfile(string LoanerName, int LoanedID, double LoanAmount, double AnnualIncome, int IDCardNo)
+        {
+            this.LoanerName = LoanerName;
+            this.LoanedID = LoanedID;
+            this.LoanAmount = LoanAmount;
+            this.AnnualIncome = AnnualIncome;
+            this.IDCardNo = IDCardNo;
+        }
+
+    }
+
+    public class LoanProcessor
+    {
+        public void LoanDetails()
+        {
+            LoanProfile loanProfile = new LoanProfile("John Doe", 12345, 500000, 1000000, 987654321);
+            bool flag = true;
+
+            if (string.IsNullOrEmpty(loanProfile.LoanerName) || string.IsNullOrEmpty("" + loanProfile.LoanAmount) || string.IsNullOrEmpty("" + loanProfile.LoanedID) || string.IsNullOrEmpty("" + loanProfile.IDCardNo) || string.IsNullOrEmpty("" + loanProfile.AnnualIncome))
+                flag = false;
+
+            if (flag)
+                Console.WriteLine($"Loan to {loanProfile.LoanerName}  of Amount Rs {loanProfile.LoanAmount} has been sanctioned.");
+            else
+                Console.WriteLine("Loan Cannot be Sanctioned!!");
+
+            loanProfile = null;
+
+        }
+
+
+    }
+}
