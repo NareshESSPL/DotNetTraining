229e36fa5566bb24370234c123b2aa0b9be0f44e
Preetinandita Pradhan <preetinandita@essbbsr.esspl.com>
2025-04-29 15:32:21 +0530

    Assignment

---
+++ Preeti/Assignment1/Program.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeManagementSystem;
+
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Test t = new Test();
+            t.testing();
+        }
+    }
+}
---
+++ Preeti/Assignment1/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem
+{
+    public class Test
+    {
+        public void testing ()
+        {
+            string empType = "ContractEmployee";
+            HumanResource humanResource = new HumanResource("ContractEmployee");
+            double salary = humanResource.calculateFinalSalary();
+            double salaryWithPfa = humanResource.calculateFinalSalary(500);
+            Console.WriteLine("the salary of " + empType + " is :" + salary);
+            Console.WriteLine("the salary with pfa " + empType + " is:" + salaryWithPfa);
+
+            empType = "PermanentEmployee";
+            humanResource = new HumanResource("PermanentEmployee");
+            double salary1 = humanResource.calculateFinalSalary();
+            double salaryWithPfa1 = humanResource.calculateFinalSalary(500);
+            Console.WriteLine("the salary of " + empType + "is :" + salary1);
+            Console.WriteLine("the salary with pfa " + empType + "is:" + salaryWithPfa1);
+        }
+    }
+}
