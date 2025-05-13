5f01565ecb1ed147439759f0d60e3b0753074d3b
Aditya Kumar Mohanty <adityam@essbbsr.esspl.com>
2025-05-13 14:02:10 +0530

    Student Management

---
+++ Aditya/StdentManagement/StdentManagement/Controllers/StudentController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using StudentManagement.DataLayer;
+using StudentManagementSystem.DTO;
+
+namespace StdentManagement.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<HomeController> _logger;
+        private readonly StudentDAL _studentDAL;
+
+        public StudentController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _studentDAL = new StudentDAL(_configuration);
+        }
+        public IActionResult Index()
+        {
+            var student = _studentDAL.GetStudents();
+            return View(student);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Submit(Student student)
+        {
+            ModelState.Remove("StudentID");
+            if (ModelState.IsValid)
+            {
+                _studentDAL.AddStudent(student);
+                return RedirectToAction("Index");
+            }
+
+            
+
+            return View("Create", student);
+        }
+
+        public IActionResult edit(int id)
+        {
+
+            var student = _studentDAL.GetStudent(id);
+            return View("Create", student);
+        }
+
+        public IActionResult delete(int id)
+        {
+            _studentDAL.DeleteStudent(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            var student = _studentDAL.GetStudent(id);
+            return View(student);
+        }
+
+
+
+    }
+}
---
+++ Aditya/StdentManagement/StudentManagement.DataLayer/StudentDAL.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using StudentManagementSystem.DTO;
+using System.Data;
+using System.Data.SqlTypes;
+namespace StudentManagement.DataLayer
+{
+    public class StudentDAL
+    {
+        
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionstring;
+
+        public StudentDAL(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionstring = _configuration.GetConnectionString("ConString");
+        }
+
+        public List<Student> GetStudents()
+        {
+            var students = new List<Student>();
+
+            using (var sqlCon = new SqlConnection(_connectionstring))
+            {
+                using (var sqlCmd = new SqlCommand("SELECT * FROM Student", sqlCon))
+                {
+                    sqlCmd.CommandType = CommandType.Text;
+
+                    sqlCon.Open();
+
+                    var reader = sqlCmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        var student = new Student();
+                        student.StudentID = reader.GetInt32(reader.GetOrdinal("StudentID"));
+                        student.FirstName = reader.GetString(reader.GetOrdinal("FirstName"));
+                        student.LastName = reader.GetString(reader.GetOrdinal("LastName"));
+                        student.DateOfBirth = reader.GetDateTime(reader.GetOrdinal("DateOfBirth"));
+                        student.Email = reader.GetString(reader.GetOrdinal("Email"));
+                        student.PhoneNumber = reader.GetString(reader.GetOrdinal("PhoneNumber"));
+                        student.Address = reader.GetString(reader.GetOrdinal("Address"));
+
+                        students.Add(student);
+                    }
+                }
+            }
+
+            return students;
+        }
+
+        public void AddStudent(Student student)
+        {
+            using (var sqlCon = new SqlConnection(_connectionstring))
+            {
+                using (var sqlCmd = new SqlCommand("AddStudent", sqlCon))
+                {
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+
+                    sqlCmd.Parameters.Add(new SqlParameter("@StudentID", student.StudentID));
+                    sqlCmd.Parameters.Add(new SqlParameter("@FirstName", student.FirstName));
+                    sqlCmd.Parameters.Add(new SqlParameter("@LastName", student.LastName));
+                    sqlCmd.Parameters.Add(new SqlParameter("@DateOfBirth", student.DateOfBirth));
+                    sqlCmd.Parameters.Add(new SqlParameter("@Email", student.Email));
+                    sqlCmd.Parameters.Add(new SqlParameter("@PhoneNumber", student.PhoneNumber));
+                    sqlCmd.Parameters.Add(new SqlParameter("@Address", student.Address));
+
+                    sqlCon.Open();
+                    sqlCmd.ExecuteNonQuery();
+                }
+            }
+
+        }
+
+        public Student GetStudent(int studentId)
+        {
+            var student = new Student();
+
+            using (var sqlCon = new SqlConnection(_connectionstring))
+            {
+                using (var sqlCmd = new SqlCommand("SELECT * FROM Student WHERE StudentID =" + studentId, sqlCon))
+                {
+                    sqlCmd.CommandType = CommandType.Text;
+
+                    sqlCon.Open();
+
+                    var reader = sqlCmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        student.StudentID = reader.GetInt32(reader.GetOrdinal("StudentID"));
+                        student.FirstName = reader.GetString(reader.GetOrdinal("FirstName"));
+                        student.LastName = reader.GetString(reader.GetOrdinal("LastName"));
+                        student.DateOfBirth = reader.GetDateTime(reader.GetOrdinal("DateOfBirth"));
+                        student.Email = reader.GetString(reader.GetOrdinal("Email"));
+                        student.PhoneNumber = reader.GetString(reader.GetOrdinal("PhoneNumber"));
+                        student.Address = reader.GetString(reader.GetOrdinal("Address"));
+                    }
+                }
+            }
+
+            return student;
+        }
+
+
+        public void DeleteStudent(int studentId)
+        {
+            using (var sqlCon = new SqlConnection(_connectionstring))
+            {
+                using (var sqlCmd = new SqlCommand("DELETE FROM Student WHERE StudentID = @StudentID", sqlCon))
+                {
+                    sqlCmd.CommandType = CommandType.Text;
+                    sqlCmd.Parameters.Add(new SqlParameter("@StudentID", studentId));
+
+                    sqlCon.Open();
+                    sqlCmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+
+    }
+}
