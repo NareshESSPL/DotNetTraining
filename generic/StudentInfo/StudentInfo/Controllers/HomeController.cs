845386c5b70403fa4ecfc7959281892c0f79c6a6
Mrutyunjaya Satapathy <mrutyunjaya@essbbsr.esspl.com>
2025-05-14 15:08:29 +0530

    StudentOperation

---
+++ generic/StudentInfo/StudentInfo/Controllers/HomeController.cs
@@ -0,0 +1,167 @@
+﻿
+using System.Data;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using StudentInfo.Models;
+
+namespace StudentInfo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            var students = GetStudentsFromDatabase();
+            return View(students);
+        }
+        private List<StudentModel> GetStudentsFromDatabase()
+        {
+            List<StudentModel> students = new List<StudentModel>();
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * from Student";
+                SqlCommand cmd = new SqlCommand(query, con);
+                con.Open();
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        students.Add(new StudentModel
+                        {
+                            StudentId = (int)reader["StudentID"],
+                            FirstName = reader["FirstName"].ToString(),
+                            LastName = reader["LastName"].ToString(),
+                            DateOfBirth = Convert.ToDateTime(reader["DateOfBirth"]),
+                            Gender = reader["Gender"].ToString(),
+                            Email = reader["Email"].ToString()
+                        });
+                    }
+                }
+            }
+            return students;
+        }
+        public IActionResult Create()
+        {
+            return View();  
+        }
+        [HttpPost]
+        public IActionResult Create(StudentModel student)
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("InsertStudent", con);
+                cmd.CommandType = CommandType.StoredProcedure;  
+
+                cmd.Parameters.AddWithValue("@FirstName", student.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", student.LastName);
+                cmd.Parameters.AddWithValue("@DateOfBirth", student.DateOfBirth);
+                cmd.Parameters.AddWithValue("@Gender", student.Gender);
+                cmd.Parameters.AddWithValue("@Email", student.Email);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            StudentModel student = GetStudentById(id);
+            return View(student);
+        }
+        [HttpPost]
+        public IActionResult Edit(StudentModel student)
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            using(SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("UpdateStudent",con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@StudentId", student.StudentId);
+                cmd.Parameters.AddWithValue("@FirstName", student.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", student.LastName);
+                cmd.Parameters.AddWithValue("@DateOfBirth", student.DateOfBirth);
+                cmd.Parameters.AddWithValue("@Gender", student.Gender);
+                cmd.Parameters.AddWithValue("@Email", student.Email);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            return RedirectToAction("Index");
+        }
+
+        private StudentModel GetStudentById(int id)
+        {
+            StudentModel student = null;
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using(SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Student WHERE StudentID = @id";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                conn.Open();
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        student = new StudentModel
+                        {
+                            StudentId = (int)reader["StudentID"],
+                            FirstName = reader["FirstName"].ToString(),
+                            LastName = reader["LastName"].ToString(),
+                            DateOfBirth = Convert.ToDateTime(reader["DateOfBirth"]),
+                            Gender = reader["Gender"].ToString(),
+                            Email = reader["Email"].ToString()
+                        };
+                    }
+                }
+            }
+            return student;
+        }
+
+        
+        public IActionResult Delete(int id)
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            using(SqlConnection conn = new SqlConnection( connectionString))
+            {
+               
+                
+                SqlCommand cmd = new SqlCommand("DeleteStudent", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@StudentId", id);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ generic/StudentInfo/StudentInfo/Models/StudentModel.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentInfo.Models
+{
+    public class StudentModel
+    {
+       public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+
+    }
+}
