4c623f02cb7c58f96d353e98fd54080d6b0d2f0c
Shresth Gaurav <shresthsingh09aug@gmail.com>
2025-05-26 01:15:02 +0530

    ASSS

---
+++ Assignment/data_fIRSTTT/Program.cs
@@ -0,0 +1,41 @@
+using data_fIRSTTT.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace data_fIRSTTT
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            var provider=builder.Services.BuildServiceProvider();
+            var config=provider.GetService<IConfiguration>();
+            builder.Services.AddDbContext<StudentContext>(item => item.UseSqlServer(config.GetConnectionString("constr")));
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
