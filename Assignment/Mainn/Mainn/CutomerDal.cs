44f2b1488fa28502130bb0e06750aa5d25cdca42
Nitish Kumar <nitishk@essbbsr.esspl.com>
2025-05-29 21:49:07 +0530

    assignment

---
+++ Assignment/Mainn/Mainn/CutomerDal.cs
@@ -0,0 +1,67 @@
+﻿using System.Data.SqlClient;
+using System.Security.Cryptography.X509Certificates;
+using Help;
+
+namespace Mainn
+{
+    public class CutomerDal
+    {
+        private readonly string _connectionString;
+        public CutomerDal(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public void AddCutomer(hey customer)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var query = "INSERT INTO Customers (Name, Age, Address, OrderId) VALUES (@Name, @Age, @Address, @OrderId)";
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Name", customer.Name);
+                    command.Parameters.AddWithValue("@Age", customer.Age);
+                    command.Parameters.AddWithValue("@Address", customer.Address);
+                    command.Parameters.AddWithValue("@OrderId", customer.OrderId);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        public void UpdateCutomer(hey customer)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var query = "UPDATE Customers SET Name = @Name, Age = @Age, Address = @Address WHERE OrderId = @OrderId";
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Name", customer.Name);
+                    command.Parameters.AddWithValue("@Age", customer.Age);
+                    command.Parameters.AddWithValue("@Address", customer.Address);
+                    command.Parameters.AddWithValue("@OrderId", customer.OrderId);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        public void DeleteCutomser(hey customer)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var query = "DELETE FROM Customers WHERE OrderId = @OrderId";
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@OrderId", customer.OrderId);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
+    
+
+            
+       
+    
+
+
