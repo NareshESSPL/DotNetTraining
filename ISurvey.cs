4c68cd101e1f1378df5ca9275a6807e2468d4e13
khus158 <159310438+khus158@users.noreply.github.com>
2025-06-10 17:22:08 +0530

    Add files via upload

---
+++ ISurvey.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using Survey.Models;
+
+namespace Survey.Interfaces
+{
+    public interface ISurvey
+    {
+        void AddSurvey(Survey survey);
+        void UpdateSurvey(Survey survey);
+        void DeleteSurvey(int surveyId);
+        List<Survey> GetAllSurveys();
+    }
+}
---
+++ SurveyBase.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+namespace Survey.Models.DTOs
+{
+    public abstract class SurveyBase
+    {
+        public int SurveyID { get; set; }
+        public int SurveyTypeID { get; set; }
+        public string SurveyTypeName { get; set; }
+        public string ModifiedBy { get; set; }
+        public string ModifiedDate { get; set; }
+
+        public abstract void DisplaySurveyDetails();
+    }
+}
+
