43cdca1a72ecfa629ec5f28b12160776f5bc8a18
Mrutyunjaya Satapathy <mrutyunjaya8658@gmail.com>
2025-06-10 17:20:54 +0530

    Exam folder update

---
+++ CSharpExam/DTO/Program.cs
@@ -0,0 +1,35 @@
+﻿using SurveyManagement.DTO;
+
+namespace SurveyManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SurveyManager surveyManager = new SurveyManager();
+
+            surveyManager.SurveyTypeID = 3;
+            surveyManager.SurveyTypeName = "unit21";
+            surveyManager.ModifiedBy = "user31";
+
+            surveyManager.AddSurvey(surveyManager);
+
+            var newsurveyManager = new SurveyManager()
+            {
+                SurveyTypeID = 2,
+                SurveyTypeName = "Group",
+                ModifiedBy = "admin"
+            };
+
+            surveyManager.UpdateSurvey(newsurveyManager);
+
+            surveyManager.DeleteSurvey(surveyManager.SurveyTypeID);
+
+            surveyManager.logger(surveyManager);
+
+
+
+
+        }
+    }
+}
