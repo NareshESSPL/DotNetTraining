ae4c92dd15bf5e95ac5223e044bb0a97779fa100
Mrutyunjaya Satapathy <mrutyunjaya@essbbsr.esspl.com>
2025-06-18 18:02:32 +0530

    Initial commit

---
+++ EFCoreExample/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using EFCoreExample.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreExample.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly AppDbContext _context;
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+        //public async Task<IActionResult> Index()
+        //{
+        //    var users = await _context.User.ToListAsync();
+        //    return View(users);
+        //}
+
+        public IActionResult Index()
+        {
+            var users =  _context.User.ToList();
+            return View(users);
+            
+        }
+    }
+}
---
+++ EFCoreExample/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using EFCoreExample.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreExample.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<User> User { get; set; }
+    }
+}
