3ccdc32e8a5ae0c4d3ea2f2bf5e3fc02c3255afb
Jayadev Patra <jayadev@essbbsr.esspl.com>
2025-06-18 20:35:39 +0530

    Efcore

---
+++ EFCoreDemo/EFCoreDemo/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using EFCoreDemo.Data;
+using EFCoreDemo.Models;
+
+namespace EFCoreDemo.Controllers
+{   
+    public class StudentController : Controller
+    {
+    public ApplicationDbContext _context;
+
+    public StudentController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+        public IActionResult Index()
+        {
+            List<Student> students = _context.Students.ToList();
+            return View(students);
+        }
+    }
+}
---
+++ EFCoreDemo/EFCoreDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+using EFCoreDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace EFCoreDemo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+          : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
