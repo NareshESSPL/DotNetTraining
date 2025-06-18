e9fcb5257600ba17fa658dd4c37efd72061d6700
Preetinandita Pradhan <preetinandita@essbbsr.esspl.com>
2025-06-18 19:07:23 +0530

    Efcode

---
+++ Efcode/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using Efcode.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Efcode.Controllers
+{
+    public class StudentController : Controller
+    {    
+        private readonly AppDbContext _appDbContext;
+        public StudentController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public IActionResult Index()
+        {  
+            var users = _appDbContext.students.ToList();
+            return View(users);
+        }
+    }
+}
---
+++ Efcode/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Efcode.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Efcode.Data
+{
+    public class AppDbContext:DbContext
+    {
+       public AppDbContext(DbContextOptions<AppDbContext>options):base(options) 
+        {
+
+        }
+        public DbSet<Student> students { get; set; }
+    }
+}
