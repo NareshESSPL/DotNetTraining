b5395ec5f8285ce5f79a4a52c2ca11f4db97ff47
Aditya Shukla <shuklaaditya5805@gmail.com>
2025-06-19 10:37:26 +0530

    done

---
+++ Aditya/Efcore/Efcore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Efcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Aditya/Efcore/Entity/User.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+    }
+}
