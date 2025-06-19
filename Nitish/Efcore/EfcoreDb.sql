5ad44f9bd05db282bbde2fda0557a348823b9154
Nitish Kumar Sahoo <nitishs@essbbsr.esspl.com>
2025-06-19 11:01:22 +0530

    done

---
+++ Nitish/Efcore/EfcoreDb.sql
@@ -0,0 +1,14 @@
+Create DataBase Entity;
+
+use Entity;--ESSPLLAP290\SQL2019
+
+CREATE TABLE [User] (
+    Id INT PRIMARY KEY
+);
+
+go 
+
+INSERT INTO [User] (Id) VALUES (1);
+INSERT INTO [User] (Id) VALUES (2);
+INSERT INTO [User] (Id) VALUES (3);
+
